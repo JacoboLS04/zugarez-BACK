45879c4752d259af2646ec42db8bb73dc1506732
Jacobo Luengas <162923456+JacoboLS04@users.noreply.github.com>
2025-09-07 12:44:24 -0500

    Versionamiento de Beans para deploy

--- src/main/java/com/zugarez/zugarez_BACK/config/PrometheusConfig.java
+++ src/main/java/com/zugarez/zugarez_BACK/config/PrometheusConfig.java
@@ -1,16 +1,16 @@
 package com.zugarez.zugarez_BACK.config;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 
 @Configuration
 public class PrometheusConfig {
 
     @Bean
     public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
-        return registry -> registry.config().commonTags(
+        return (MeterRegistry registry) -> registry.config().commonTags(
             "application", "zugarez-backend",
             "environment", "production",
             "platform", "koyeb"
