dcf4151133f7e6b2e43e89ca74e4425de39137a5
Jacobo Luengas <162923456+JacoboLS04@users.noreply.github.com>
2025-09-07 15:27:15 -0500

    Map.of() fuera

--- src/main/java/com/zugarez/zugarez_BACK/controller/PrometheusController.java
+++ src/main/java/com/zugarez/zugarez_BACK/controller/PrometheusController.java
@@ -1,8 +1,5 @@
 package com.zugarez.zugarez_BACK.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusMeterRegistry;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,10 +14,10 @@ public class PrometheusController {
     public ResponseEntity<Map<String, Object>> query(@RequestParam String query) {
         Map<String, Object> response = new HashMap<>();
         response.put("status", "success");
-        response.put("data", Map.of(
-            "resultType", "vector",
-            "result", new Object[]{}
-        ));
+        Map<String, Object> data = new HashMap<>();
+        data.put("resultType", "vector");
+        data.put("result", new Object[]{});
+        response.put("data", data);
         return ResponseEntity.ok(response);
     }
 
@@ -33,14 +30,17 @@ public class PrometheusController {
     public ResponseEntity<Map<String, Object>> buildinfo() {
         Map<String, Object> response = new HashMap<>();
         response.put("status", "success");
-        response.put("data", Map.of(
-            "version", "2.0.0",
-            "revision", "spring-boot-actuator",
-            "branch", "main",
-            "buildUser", "spring-boot",
-            "buildDate", "2024-01-01T00:00:00Z",
-            "goVersion", "go1.21"
-        ));
+        Map<String, Object> data = new HashMap<>();
+        data.put("version", "2.0.0");
+        data.put("revision", "spring-boot-actuator");
+        data.put("branch", "main");
+        data.put("buildUser", "spring-boot");
+        data.put("buildDate", "2024-01-01T00:00:00Z");
+        data.put("goVersion", "go1.21");
+        response.put("data", data);
+        return ResponseEntity.ok(response);
+    }
+}
         return ResponseEntity.ok(response);
     }
 }
