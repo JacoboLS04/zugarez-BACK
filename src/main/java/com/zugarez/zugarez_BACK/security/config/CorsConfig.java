07f045b6076560c0547e56fa4c957ea23e845e8a
Jacobo Luengas <162923456+JacoboLS04@users.noreply.github.com>
2025-09-07 14:10:33 -0500

    Dios mio por favor no mas

--- src/main/java/com/zugarez/zugarez_BACK/security/config/CorsConfig.java
+++ src/main/java/com/zugarez/zugarez_BACK/security/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Bean(name = "securityCorsConfig")
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
