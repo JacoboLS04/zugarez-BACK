fa89f08d62b965cc3df88247c364472efbd6d12f
Jacobo Luengas <162923456+JacoboLS04@users.noreply.github.com>
2025-09-07 12:12:50 -0500

    Cambios en las rutas estaticas

--- src/main/java/com/zugarez/zugarez_BACK/config/SecurityConfig.java
+++ src/main/java/com/zugarez/zugarez_BACK/config/SecurityConfig.java
@@ -11,9 +11,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain monitoringSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-            .securityMatcher("/actuator/**", "/monitoring/**") // Maneja solo estas rutas
+            .securityMatcher("/actuator/**", "/monitoring/**", "/", "/auth/**", "/static/**") // Incluye rutas del frontend y autenticación
             .authorizeHttpRequests()
-                .requestMatchers("/actuator/prometheus", "/monitoring/health").permitAll() // Permitir acceso público
+                .requestMatchers("/actuator/prometheus", "/monitoring/health").permitAll() // Permitir acceso público al monitoreo
+                .requestMatchers("/", "/auth/**", "/static/**").permitAll() // Permitir acceso público al frontend y autenticación
                 .anyRequest().authenticated() // Proteger el resto de las rutas
             .and()
             .csrf().disable(); // Deshabilitar CSRF para simplificar pruebas
