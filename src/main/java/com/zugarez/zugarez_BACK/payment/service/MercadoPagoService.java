73221b4c70576e0959d961c09eba7a661205fe5f
Jacobo Luengas <162923456+JacoboLS04@users.noreply.github.com>
2025-10-19 13:52:20 -0500

    Update MercadoPagoService.java

--- src/main/java/com/zugarez/zugarez_BACK/payment/service/MercadoPagoService.java
+++ src/main/java/com/zugarez/zugarez_BACK/payment/service/MercadoPagoService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import jakarta.annotation.PostConstruct;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
